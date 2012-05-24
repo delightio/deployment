39f4ef8db4404d479e927f8fcc4ffe2513b0ef31
delight <heroku@pipelyinc.com>
2012-05-23 19:56:04 -0500

    Take branches as arguments.

--- deploy_gesturedrawer.sh
+++ deploy_gesturedrawer.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 # This script builds and deploys the latest version of the gesturedrawer.
+# Usage: deploy_gesturedrawer <delight.web branch> <gesturedrawer branch>
 
-ROOT_UID="0"
+# Check that user is delight since it messes up permissions otherwise
+if [ "$USER" != "delight" ] ; then
+	echo "This script must be run as delight user."
+	exit 1
+fi
 
-# Check that user has sudo
-# if [ "$UID" -ne "$ROOT_UID" ] ; then
-#	echo "This script needs sudo to run."
-#	exit 1
-# fi
+web_branch=${1-master}
+draw_branch=${2-master}
 
 # Get the latest code
 cd /Users/delight/code/delight.web
+git checkout $web_branch || { echo "can't switch to delight.web branch $web_branch"; exit 2; }
 git pull || { echo "git pull failed, aborting"; exit 2; }
 cd /Users/delight/code/gesturedraw
+git checkout $draw_branch || { echo "can't switch to gesturedrawer branch $draw_branch"; exit 2; }
 git pull || { echo "git pull failed, aborting"; exit 2; }
 
 # Build it
