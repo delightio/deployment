c89ed9813a2167ec8c604e6b0a73f7922ccfa1f1
Chris Haugli <chris@nowbox.com>
2012-05-24 18:24:54 -0700

    Separate scripts for master and staging.

--- deploy_gesturedrawer_remote.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This script deploys gesturedrawer remotely via SSH.
-# Usage: deploy_gesturedrawer_remote.sh <delight.web branch> <gesturedrawer branch>
-
-echo "When prompted, enter delight user password."
-ssh -t delight@b11-4.macminivault.com "~/code/deployment/deploy_gesturedrawer.sh" "$1" "$2"
-
